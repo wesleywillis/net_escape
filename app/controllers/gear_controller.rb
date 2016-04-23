d10f0d13897eacfc9fdb8d2671d4be65dd8d537e
Wesley Willis <haileyw@gmail.com>
2016-04-23 12:11:59 -0700

    add seed data

--- app/controllers/gear_controller.rb
+++ app/controllers/gear_controller.rb
@@ -4,4 +4,23 @@ class GearController < ApplicationController
     @found_gear = Gear.where(found: true)
     @lost_gear = Gear.where(lost: true)
   end
+
+  def new
+    @gear = Gear.new
+  end
+
+  def create
+    @gear = Gear.new(gear_params
+    if @gear.save
+      redirect_to root_path
+    else
+      render "new"
+    end    
+  end
+
+  private
+
+  def gear_params
+    params.require(:gear).permit(:gear_type, :quantity, :color, :wildlife, :wildlife_type, :wildlife_quantity, :latitude, :longitude, :lost, :found)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_gear = [
+  {gear_type: "mesh net", quantity: 1, color: "red", wildlife: false, latitude: 25.373809, longitude: -30.585938, lost: true},
+  {gear_type: "trawler", quantity: 4, color: "brown", wildlife: false, latitude: -36.412442, longitude: -101.250000, lost: true},
+  {gear_type: "lobster trap", quantity: 10, color: "black", wildlife: false, latitude: -35.844535, longitude: -97.734375, found: true},
+  {gear_type: "fishing pole", quantity: 15, color: "multi", wildlife: false, latitude: 39.724089, longitude: 170.859375, lost: true},
+  {gear_type: "mesh net", quantity: 3, color: "yellow", wildlife: true, wildlife_type: "birds, salmon", wildlife_quantity: 1200, latitude: 35.263562, longitude: -177.187500, found: true},
+]
+
+seed_gear.each do |seed|
+  Gear.create(seed)
+end
