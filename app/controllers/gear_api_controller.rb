59e1630bf9b24cc3c9eb79c716e7c55280eefc75
Wesley Willis <haileyw@gmail.com>
2016-04-23 11:28:27 -0700

    add routes to gear

--- app/controllers/gear_api_controller.rb
+++ app/controllers/gear_api_controller.rb
@@ -1,3 +1,12 @@
 class GearApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'welcome#index'
+
+  resources :gear
 end
