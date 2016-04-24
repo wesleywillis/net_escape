7de76dc140bd68ec1d8ec439144c414049d23dfe
Wesley Willis <haileyw@gmail.com>
2016-04-23 18:42:13 -0700

    added json formats to create and index

--- app/controllers/gear_controller.rb
+++ app/controllers/gear_controller.rb
@@ -1,57 +1,46 @@
 class GearController < ApplicationController
   def index
     @all_gear = Gear.all
-    puts "$*$*" * 20
-    @found_gear = Gear.where(lost: "0")
-    puts @found_gear
-    @lost_gear = Gear.where(lost: "1")
-    puts @lost_gear
-    puts "$*$*" * 20
+    @found_gear = Gear.where(found: true)
+    @lost_gear = Gear.where(lost: true)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @all_gear }
+    end
   end
 
   def new
     @status = params[:status]
-    if @status === "0"
-      @found = "1"
-      @header = "Thank you for reporting a FOUND item"
+    if @status == '1'
+      @header = "Thank you for reporting a found item"
     else
-      @found = "0"
-      @header = "Thank you for reporting a LOST item"
+      @header = "Thank you for reporting a lost item"
     end
-    puts "$#$#" * 20
-    puts @status.class
-    puts "$#$#" * 20
+    puts "$$" * 20
+    puts @status
+    puts "$$" * 20
+
     @gear = Gear.new
   end
 
   def create
-    puts "$$" * 20
-    puts params[:lost]
-    puts "$$" * 20
     @gear = Gear.new(gear_params)
     if @gear.save
       redirect_to root_path
     else
       render "new"
     end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @gear }
+    end
+
   end
-
-
-  def found
-    gear = Gear.find(params[:id])
-    gear.found = "1"
-    gear.lost = "0"
-    gear.save
-    redirect_to '/'
-  end
-
-
 
   private
   def gear_params
-    puts "%%%%" * 30
-    puts params[:gear][:lost]
-    puts params[:gear][:lost].class
     params.require(:gear).permit(:gear_type, :gear_quantity, :color, :wildlife, :wildlife_type, :wildlife_quantity, :latitude, :longitude, :lost, :found)
   end
 end
